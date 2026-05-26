eeda8476b4917008c38c5a6d296f098ae7a05ba0
Bram <ingoa2000@hotmail.com>
2026-05-25 21:49:02 -0600

    UPDATE DataFrame.py

--- Practicas_2/DataFrame.py
+++ Practicas_2/DataFrame.py
@@ -9,40 +9,38 @@ sqft_lot = list(df['sqft_lot'])
 floors = list(df['floors'])
 sqft_above = list(df['sqft_above'])
 
-def media(list):
-    sum = 0
-    for i in list:
-        sum += i
-    m = sum/(len(list))
-    return m
+def media(lista):
+    suma = 0
+    for i in lista:
+        suma += i
+    return suma / len(lista)
 
-def moda(list):
-    moda = {}
-    max = 0
-    for i in list:
-        valor = int(list.count(i))
-        moda[i] = valor
-        if moda[i] > max:
-            max = moda[i]
-            x = i
-    return x
+def moda(lista):
+    frecuencias = {}
+    max_frec = 0
+    moda_valor = None
+    for i in lista:
+        frecuencias[i] = frecuencias.get(i, 0) + 1
+        if frecuencias[i] > max_frec:
+            max_frec = frecuencias[i]
+            moda_valor = i
+    return moda_valor
 
-def varianza(list):
-    sumV = 0
-    for i in list:
-        sumV += ((i - media(list))**2)  
-    v = sumV/len(list)
-    return v
+def varianza(lista):
+    m = media(lista)   # calcular una sola vez ya que el anterior me tardaba mucho
+    suma = 0
+    for i in lista:
+        suma += (i - m) ** 2
+    return suma / len(lista)
 
-def desv(list):
-    d = varianza(list)
-    return d**0.5
+def desv(lista):
+    return varianza(lista) ** 0.5
 
 def datos_resultantes(lista):
-    print (media(lista))
-    print (moda(lista))
-    print (varianza(lista))
-    print (desv(lista),'\n')
+    print("Media:", media(lista))
+    print("Moda:", moda(lista))
+    print("Varianza:", varianza(lista))
+    print("Desviación estándar:", desv(lista), '\n')
 
 datos_resultantes(price)
 datos_resultantes(bathrooms)
@@ -50,8 +48,3 @@ datos_resultantes(sqft_living)
 datos_resultantes(sqft_lot)
 datos_resultantes(floors)
 datos_resultantes(sqft_above)
-
-    
-
-
-    
